07e9e6c540f9b6a1cea00bb4976d666be0bcb437
Arber <88089607+ArberTu474@users.noreply.github.com>
2023-07-16 12:11:54 +0200

    Added some changes to resolve #42

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,10 +2,10 @@
 module.exports = {
   content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
   theme: {
+    fontFamily: {
+      sans: ['inter', 'system-ui'],
+    },
     extend: {
-      fontFamily: {
-        inter: ['Inter', 'sans-serif'],
-      },
       screens: {
         xs: '550px',
       },
