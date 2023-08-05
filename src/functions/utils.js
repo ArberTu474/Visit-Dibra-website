0e84091c438e3a9874b793c47b30f8d8a4f362f5
ArberTu474 <arber.tusha@outlook.com>
2023-08-05 16:49:41 +0200

    Added accommodation type icons

--- src/functions/utils.js
+++ src/functions/utils.js
@@ -1,3 +1,11 @@
 export function formatDate(date) {
   return new Date(date).toLocaleDateString()
 }
+
+export function headerRenderer(str) {
+  let frags = str.split('_');
+  for (let i = 0; i < frags.length; i++) {
+    frags[i] = frags[i].charAt(0).toUpperCase() + frags[i].slice(1);
+  }
+  return frags.join(' ');
+}
