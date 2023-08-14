a9bb8a96ffd3263536e3bc8a273eebc2a6a69f94
ArberTu474 <arber.tusha@outlook.com>
2023-08-14 15:29:28 +0200

    Added tags and author pages

--- src/functions/utils.js
+++ src/functions/utils.js
@@ -3,9 +3,20 @@ export function formatDate(date) {
 }
 
 export function headerRenderer(str) {
-  let frags = str.split('_');
+  let frags = str.split('_')
   for (let i = 0; i < frags.length; i++) {
-    frags[i] = frags[i].charAt(0).toUpperCase() + frags[i].slice(1);
+    frags[i] = frags[i].charAt(0).toUpperCase() + frags[i].slice(1)
   }
-  return frags.join(' ');
+  return frags.join(' ')
+}
+
+export function slugify(str) {
+  return String(str)
+    .normalize('NFKD') // split accented characters into their base characters and diacritical marks
+    .replace(/[\u0300-\u036f]/g, '') // remove all the accents, which happen to be all in the \u03xx UNICODE block.
+    .trim() // trim leading or trailing whitespace
+    .toLowerCase() // convert to lowercase
+    .replace(/[^a-z0-9 -]/g, '') // remove non-alphanumeric characters
+    .replace(/\s+/g, '-') // replace spaces with hyphens
+    .replace(/-+/g, '-') // remove consecutive hyphens
 }
