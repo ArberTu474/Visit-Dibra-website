aa5e7599c6eaad2c6e3da24f77bdb23f9508f65d
ArberTu474 <arber.tusha@outlook.com>
2023-08-25 18:53:13 +0200

    Added the head information for the hotel rooms

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,2 +1,7 @@
 /// <reference path="../.astro/types.d.ts" />
 /// <reference types="@astrojs/image/client" />
+/// <reference types="astro/client" />
+interface ImportMetaEnv {
+  readonly APIKEY: string
+  readonly AUTHORIZATION: string
+}
